0a8576fa49c3f2b425d0c17bdee3b66a22e9d2a0
c0a2420223 <c0a2420223@edu.teu.ac.jp>
2025-04-29 15:14:04 +0900

    bomb

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -7,6 +7,7 @@ import pygame as pg
 
 WIDTH = 1100  # ゲームウィンドウの幅
 HEIGHT = 650  # ゲームウィンドウの高さ
+NUM_OF_BOMBS=5
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -147,6 +148,9 @@ def main():
     bird = Bird((300, 200))
     beam = None
     bomb = Bomb((255, 0, 0), 10)
+    bombs = []
+    for i in range(NUM_OF_BOMBS):
+        bombs.append(Bomb((255, 0, 0), 10))
     clock = pg.time.Clock()
     tmr = 0
     while True:
@@ -157,24 +161,25 @@ def main():
             #     # スペースキー押下でBeamクラスのインスタンス生成
                 beam = Beam(bird)            
         screen.blit(bg_img, [0, 0])
-        if bomb is not None:
+        for bomb in bombs:
             if bird.rct.colliderect(bomb.rct):
                 # ゲームオーバー時に，こうかとん画像を切り替え，1秒間表示させる
                 bird.change_img(8, screen)
                 pg.display.update()
                 time.sleep(1)
                 return
-        if beam is not None:
-            if bomb is not None:
+        for j,bomb in enumerate(bombs):
+            if beam is not None:
                 if beam.rct.colliderect(bomb.rct):
                     beam = None
-                    bomb = None
-
+                    bombs[j] = None
+                    bird.change_img(6,screen)
+                bombs=[bomb for bomb in bombs if bomb is not None]
         key_lst = pg.key.get_pressed()
         bird.update(key_lst, screen)
         if beam is not None:
             beam.update(screen)
-        if bomb is not None:
+        for bomb in bombs:
             bomb.update(screen)
         # beam.update(screen)   
         bomb.update(screen)
